46834fb4ed594b27b3dad3430bb6193e7795834d
alexkhor1983 <woshilaogu8888@gmail.com>
2024-01-23 16:54:19 +0800

    add circular progress bar and prompt optimize

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import React, { useState, useCallback } from "react";
 import { FileUploader } from "react-drag-drop-files";
-import axios from "axios"
+import axios from "axios";
 import config from "./config/api_key.json";
-
-window.addEventListener("dragover",function(e){
-  e = e || event;
-  e.preventDefault();
-},false);
-window.addEventListener("drop",function(e){
-  e = e || event;
-  e.preventDefault();
-},false);
+import CircularProgress from '@mui/joy/CircularProgress';
 
 function App() {
   const fileTypes = ["JPG", "JPEG", "PNG", "SVG"];
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [generatedText, setGeneratedText] = useState("");
 
   const handleDrop = useCallback(async (file) => {
-
     if (file) {
       const base64Data = await fileToBase64(file);
       setImages([{ src: base64Data, name: file.name }]);
@@ -37,22 +29,21 @@ function App() {
   };
 
   const handleGenerate = async () => {
-    if(images == [] || images == null){
+    if (images.length === 0 || images === null) {
       console.log("No image");
-      return
+      return;
     }
-
+  
     try {
-      const apiKey = config.apiKey; // You need to create a app_key.json and fill it with your API key such as { "apiKey":"xxxxxxxxx"}
-        
-      const prompt = "I need you act as the caption provider for this image. Do not using the name of the people even you know the name, still using man or woman to describe. Caption must be english. What is this picture caption? All the caption within 5 until 15 words. Caption end with on the floor, on the grass, in the forest, besides of item's name was accepted. Ensure the correct use of preposition (by/at/into/with)​. Ensure the correct use of pronouns (he/she/it/they/them)​. Caption can be simplified, but must relate to image​. Caption must be lower case except for names​ . No uppercase unless for proper nouns. Do not adding fullstop at the end. Ensure captions does not include subjective terms​(emotions[happy,sad,suppriced]/sizes[big,small]/looks[cute/urgly/buetiful]). Ensure captions does not include phrases (there are/ a view of/ a picture of). Avoid describing the sky unless it is focused. Please use general term for human such as children, people, women, woman, men, man. Don't assume and guessing picture object relationship such as wild/pet/human's relationships[father,mother,husband,friends]. Do not using words like mother, father, mom, girlfriend, boyfriend, numbers of friends. Using verb to describe aciton. With the case not enough words can using a, the, an. Using two, three and following numbers of men or women instead of two, three or following numbers of friends.";
+      const apiKey = config.apiKey;
+      const prompt = "I need you act as the caption provider for this image. Do not use uppercase for the first letter. Do not adding full stop at the end. No uppercase unless for proper nouns. Do not using the name of the people even you know the name, still using man or woman to describe. Caption must be english. What is this picture caption? All the caption within 5 until 15 words. Caption end with on the floor, on the grass, in the forest, besides of item's name was accepted. Ensure the correct use of preposition (by/at/into/with)​. Ensure the correct use of pronouns (he/she/it/they/them)​. Caption can be simplified, but must relate to image​. Caption must be lower case except for names​ . Ensure captions does not include subjective terms​(emotions[happy,sad,suppriced]/sizes[big,small]/looks[cute/urgly/buetiful]). Ensure captions does not include phrases (there are/ a view of/ a picture of). Avoid describing the sky unless it is focused. Please use general term for human such as children, people, women, woman, men, man. Don't assume and guessing picture object relationship such as wild/pet/human's relationships[father,mother,husband,friends]. Do not using words like mother, father, mom, girlfriend, boyfriend, numbers of friends. Using verb to describe aciton. With the case not enough words can using a, the, an. Using two, three and following numbers of men or women instead of two, three or following numbers of friends.";
       const imagePart = {
         inline_data: {
           mime_type: "image/jpeg",
           data: images[0].src,
         },
       };
-
+  
       const requestData = {
         contents: [
           {
@@ -60,27 +51,30 @@ function App() {
           },
         ],
       };
-
+  
+      setLoading(true);
+  
       const response = await axios.post(
         `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent`,
         requestData,
         {
           params: {
-            'key': apiKey
+            key: apiKey,
           },
           headers: {
             "Content-Type": "application/json",
           },
         }
       );
-
-
-      
+  
+      setLoading(false);
+  
       const generatedText = response.data?.candidates[0]?.content?.parts[0]?.text;
       console.log(generatedText);
       setGeneratedText(generatedText);
     } catch (error) {
       console.error("Error generating content:", error);
+      setLoading(false);
     }
   };
 
@@ -90,16 +84,24 @@ function App() {
     <div>
       {imageNotNull ? (
         <img
-        src={`data:image/jpeg;base64,${images[0].src}`}
-        alt={images.name}
-        style={{ maxWidth: "50%", maxHeight: "50%", margin: "5px" }}
-      />
+          src={`data:image/jpeg;base64,${images[0].src}`}
+          alt={images.name}
+          style={{ maxWidth: "50%", maxHeight: "50%", margin: "5px" }}
+        />
       ) : (
         <></>
       )}
-      
+
       <FileUploader handleChange={handleDrop} name="file" types={fileTypes} />
-      <button onClick={handleGenerate}>Generate Content</button>
+
+      {loading ? (
+        <CircularProgress />
+       ) : (
+        <button onClick={handleGenerate}>
+        Generate Caption
+      </button>
+      )}
+
       {generatedText && <div>Generated Text: {generatedText}</div>}
     </div>
   );
